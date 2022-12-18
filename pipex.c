8ec290ac0ba985f5ec9ae5de6e13fffb8612f044
Hahyeon Lee <im.madylin@gmail.com>
2022-12-18 20:44:48 +0900

    start pipex

--- pipex.c
+++ pipex.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hahlee <hahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:50:16 by hahlee            #+#    #+#             */
-/*   Updated: 2022/12/12 16:59:05 by hahlee           ###   ########.fr       */
+/*   Updated: 2022/12/18 20:44:35 by hahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,31 @@
 
 int	main(int argc, char *argv[])
 {
+	int		fds[2];
+	int		fd;
+	pid_t	pid;
+	int		status;
+
 	if (argc != 5)
 		return (0);
+	if (pipe(fds) == -1)
+		return (0); //에러 시 어떻게 처리할 것인지
+
+	//자식이 하나인 경우
+	pid = fork();
+	if (pid > 0)
+	{
+		wait(&status);
+	}
+	else if (pid == 0)
+	{
+		fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+		if (fd == -1)
+			return (0); //에러 시 어떻게 처리할 것인지
+		dup2(fd, 0);
+		dup2(fds[WRITE], 1);
+	}
+	else
+		return (0); //에러 시 어떻게 처리할 것인지
 	return (0);
 }
