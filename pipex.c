779cf29bc426538f85ef4595dd76a4941e4b556a
Hahyeon Lee <im.madylin@gmail.com>
2022-12-20 16:25:03 +0900

    add to child process funtion

--- pipex.c
+++ pipex.c
@@ -6,11 +6,67 @@
 /*   By: hahlee <hahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:50:16 by hahlee            #+#    #+#             */
-/*   Updated: 2022/12/19 17:11:35 by hahlee           ###   ########.fr       */
+/*   Updated: 2022/12/20 16:24:47 by hahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+#include <stdio.h> //
+
+int	check_command(char *com)
+{
+	//access로 커맨드 정상여부 확인
+	//$PATH의 경로를 추출해오는 방법 확인해서, 콜론을 기준으로 쪼개 명령어 앞에 붙이고 access 검증
+}
+
+int	first_child(char *file, char *com, int fds[])
+{
+	int	fd;
+
+	close(fds[READ]);
+	fd = open(file, O_RDWR | O_CREAT, 0644);
+	if (fd == -1)
+		return (-1);
+	dup2(fd, 0);
+	close(fd);
+	dup2(fds[WRITE], 1);
+	close(fds[WRITE]);
+	if (check_command(com) == -1)
+		return (-1);
+	exceve();
+	return (0);
+}
+
+int	middle_child(char *com, int fds[])
+{
+	dup2(fds[READ], 0);
+	close(fds[READ]);
+	dup2(fds[WRITE], 1);
+	close(fds[WRITE]);
+	if (check_command(com) == -1)
+		return (-1);
+	exceve();
+	return (0);
+}
+
+int	last_child(char *file, char *com, int fds[])
+{
+	int	fd;
+
+	close(fds[WRITE]);
+	fd = open(file, O_RDWR | O_CREAT, 0644);
+	if (fd == -1)
+		return (-1);
+	dup2(fd, 1);
+	close(fd);
+	dup2(fds[READ], 0);
+	close(fds[READ]);
+	if (check_command(com) == -1)
+		return (-1);
+	exceve();
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -23,18 +79,14 @@ int	main(int argc, char *argv[])
 		return (0);
 	if (pipe(fds) == -1)
 		return (0);
-	pid = fork();
+	pid = 1;
 	if (pid > 0)
 	{
-		wait(&status);
+		//부모 프로세스
 	}
 	else if (pid == 0)
 	{
-		fd = open(argv[1], O_RDWR | O_CREAT, 0644);
-		if (fd == -1)
-			return (0);
-		dup2(fd, 0);
-		dup2(fds[WRITE], 1);
+		//자식 프로세스
 	}
 	else
 		return (0);
