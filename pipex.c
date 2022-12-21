f3a92e2d2a6525567276667b0336f5e90b7f46b7
Hahyeon Lee <im.madylin@gmail.com>
2022-12-21 14:08:14 +0900

    modify fork process

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: hahlee <hahlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:50:16 by hahlee            #+#    #+#             */
-/*   Updated: 2022/12/20 16:24:47 by hahlee           ###   ########.fr       */
+/*   Updated: 2022/12/21 14:06:55 by hahlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	check_command(char *com)
 {
 	//access로 커맨드 정상여부 확인
 	//$PATH의 경로를 추출해오는 방법 확인해서, 콜론을 기준으로 쪼개 명령어 앞에 붙이고 access 검증
+	(void)com;
+	return (0);
 }
 
 int	first_child(char *file, char *com, int fds[])
@@ -34,7 +36,7 @@ int	first_child(char *file, char *com, int fds[])
 	close(fds[WRITE]);
 	if (check_command(com) == -1)
 		return (-1);
-	exceve();
+	//execve();
 	return (0);
 }
 
@@ -46,7 +48,7 @@ int	middle_child(char *com, int fds[])
 	close(fds[WRITE]);
 	if (check_command(com) == -1)
 		return (-1);
-	exceve();
+	//execve();
 	return (0);
 }
 
@@ -64,31 +66,39 @@ int	last_child(char *file, char *com, int fds[])
 	close(fds[READ]);
 	if (check_command(com) == -1)
 		return (-1);
-	exceve();
+	//execve();
 	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
 	int		fds[2];
-	int		fd;
 	pid_t	pid;
-	int		status;
+	//int		status;
+	int		i;
+
+	(void)argv;
 
 	if (argc != 5)
 		return (0);
 	if (pipe(fds) == -1)
 		return (0);
-	pid = 1;
-	if (pid > 0)
+	i = 0;
+	while (i < argc - 3)
 	{
-		//부모 프로세스
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("parents %d, %d\n", pid, i);
+		}
+		else if (pid == 0)
+		{
+			printf("child %d\n", i);
+		}
+		else
+			return (0);
+		i++;
 	}
-	else if (pid == 0)
-	{
-		//자식 프로세스
-	}
-	else
-		return (0);
+	//wait(&status);
 	return (0);
 }
